54d1b84b0cba82490dfc03a3b74667bdfdb0d027
xuanhong95 <mrh95hn@gmail.com>
2016-12-04 17:40:40 +0700

    Fix send ajax

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,15 +11,5 @@ class PagesController extends Controller
     //
     function getHomeView(){
     	return view ('pages.home');
-    }
-    function saveDatabase(){
-    	$value = \Request::get('username');
-    	dd($value);
-    	$savevalue = \App\Test::where('name', '=', $value)->first();
-    	if(!$savevalue){
-    		$savevalue = new \App\Test();
-    		$savevalue->name = $value;
-    		$savevalue->save();
-    	}
-    }
+    } 
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,40 +1,41 @@
 <html>
-	<head>
-		<title>HomePage</title>
-	</head>
-	<body>
-		<h1>This is the HomePage</h1>
-		<div>
-			<form action="home">
-				<p>Write me: <input type="text" id="writeme"></p>
-				<button id="clickme">Click me!</button>
-			</form>
-		</div>
-	</body>
-	
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script type="text/javascript" src="jquery.js"></script>
+<head>
+	<title>HomePage</title>
+	<meta name="csrf-token" content="{{ csrf_token() }}" />
+</head>
+<body>
+	<h1>This is the HomePage</h1>
+	<div>
+			<p>Write me: <input type="text" id="writeme"></p>
+			<button id="clickme">Click me!</button>
+	</div>
+</body>
 
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript">
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
 
-	<script type="text/javascript">
-
+<script type="text/javascript">
 	$("#clickme").click(function(){
-	    if($("#writeme").val() == ""){
-	        alert("abc");
-	    }
-	    else{
-	         $.ajax({
-	                    url : "/save",
-	                    type : "get",
-	                    data : {
-	                         username : $('#writeme').val()
-	                    },
-	                    success : function (result){
-	                        alert(data);
-	                    }
-	                });
-	    }
+		$.ajax({
+			url: "page?username=" + $('#writeme').val(),
+			type:"POST",
+			data: {
+				username: $('#writeme').val()
+			},
+			success:function(data){
+				alert("success")
+			},error:function(){ 
+				alert("error!!!!");
+			}
+    }); //end of ajax
 	});
-	</script>
-	
+</script>
+
 </html>
