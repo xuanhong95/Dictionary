44ff9a612628851401a0bfb880a59adba7b6bb67
xuanhong95 <mrh95hn@gmail.com>
2016-12-17 18:38:31 +0700

    fix

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -7,56 +7,57 @@ use App\Http\Requests;
 
 class DataController extends Controller
 {
-    function checkDatabase(){
-      $value = \Request::input('term');
-      $type = \Request::input('searchtype');
-      if($type=="en"){
-        $savevalue = \DB::table('anh_viet')->where('word', '=', $value)->first();
-      }else{
-        $savevalue = \DB::table('viet_anh')->where('word', '=', $value)->first();
-      }
-    	
-    	if(!$savevalue){
-    		return "Từ không tồn tại";
-    	}
-    	else{
-    		return json_encode($value);
-    	}	
-
-
-  function mobileJsonEN($term){
-    $savevalue = \DB::table('anh_viet')->where('word', '=', $term)->first();
-    $result;
+  function checkDatabase(){
+    $value = \Request::input('term');
+    $type = \Request::input('searchtype');
+    if($type=="en"){
+      $savevalue = \DB::table('anh_viet')->where('word', '=', $value)->first();
+    }else{
+      $savevalue = \DB::table('viet_anh')->where('word', '=', $value)->first();
+    }
+    
     if(!$savevalue){
       return "Từ không tồn tại";
     }
     else{
-      return json_encode($savevalue);
+      return json_encode($value);
+    }	
+
+
+    function mobileJsonEN($term){
+      $savevalue = \DB::table('anh_viet')->where('word', '=', $term)->first();
+      $result;
+      if(!$savevalue){
+        return "Từ không tồn tại";
+      }
+      else{
+        return json_encode($savevalue);
+      }
     }
-  }
 
-  function showMeaningEV($term){
-    $savevalue = \DB::table('anh_viet')->where('word', '=', $term)->first();
-    $result = $savevalue->content;
-    $search_query = $term;
-    $search_query = urlencode( $search_query );
-    $html11 = file_get_html( "https://www.google.com/search?q=".$search_query."&tbm=isch" );
-    $images = $html11->find('img');
-    if($images)
-      $image = $images[0];
-    return view('pages.ev_result', compact('result', 'term', 'image'));
-  }
+    function showMeaningEV($term){
+      $savevalue = \DB::table('anh_viet')->where('word', '=', $term)->first();
+      $result = $savevalue->content;
+      $search_query = $term;
+      $search_query = urlencode( $search_query );
+      $html11 = file_get_html( "https://www.google.com/search?q=".$search_query."&tbm=isch" );
+      $images = $html11->find('img');
+      if($images)
+        $image = $images[0];
+      return view('pages.ev_result', compact('result', 'term', 'image'));
+    }
 
-  function showMeaningVE($term){
-    $savevalue = \DB::table('viet_anh')->where('word', '=', $term)->first();
-    $result = $savevalue->content;
-    $search_query = $term;
-    $search_query = urlencode( $search_query );
-    $html11 = file_get_html( "https://www.google.com/search?q=".$search_query."&tbm=isch" );
-    $images = $html11->find('img');
-    if($images)
-      $image = $images[0];
-    return view('pages.ve_result', compact('result', 'term', 'image'));
-  }
+    function showMeaningVE($term){
+      $savevalue = \DB::table('viet_anh')->where('word', '=', $term)->first();
+      $result = $savevalue->content;
+      $search_query = $term;
+      $search_query = urlencode( $search_query );
+      $html11 = file_get_html( "https://www.google.com/search?q=".$search_query."&tbm=isch" );
+      $images = $html11->find('img');
+      if($images)
+        $image = $images[0];
+      return view('pages.ve_result', compact('result', 'term', 'image'));
+    }
 
+  }
 }
